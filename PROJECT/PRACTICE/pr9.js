97809358296a23823c540d498d2f1e2647aa003b
Abhishek2149 <abhi.jaybhaye2000@gmail.com>
2023-08-29 08:15:38 +0530

    added

--- PROJECT/PRACTICE/pr9.js
+++ PROJECT/PRACTICE/pr9.js
@@ -1,5 +1,16 @@
 let nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3;
+
+function mergeSortedArray(nums1,m,nums2,n){
 nums1 = nums1.slice(0,m);
 nums1 = nums1.concat(nums2.slice(0,n));
-nums1.sort()
-console.log(nums1)
+nums1.sort((a,b) => a-b);
+return nums1;
+}
+
+let b = mergeSortedArray(nums1,m,nums2,n)
+console.log(b)
+
+console.log(1+1)
+console.log(1+"1")
+console.log("1"+1)
+console.log("1"+"1")
